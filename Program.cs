45c47d60dd17e0297f1f59d5a0480b01df298a00
Tirumala Rao  S <tiru@live.in>
2025-04-18 16:46:45 +0530

    updatedprogram.cs

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
-using Microsoft.Azure.Functions.Worker.Builder;
+using System;
+using Azure.Storage.Blobs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-var builder = FunctionsApplication.CreateBuilder(args);
+var host = new HostBuilder()
+    .ConfigureAppConfiguration(config =>
+    {
+        // Ensure we pick up local.settings.json when running locally
+        config.AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+              .AddEnvironmentVariables();
+    })
+    .ConfigureFunctionsWebApplication()   // Use this for ASP.NET Core Integration
+    .ConfigureServices((context, services) =>
+    {
+        // Register BlobServiceClient for DI
+        var storageConn = context.Configuration["AzureWebJobsStorage"];
+        services.AddSingleton(_ => new BlobServiceClient(storageConn));
+    })
+    .Build();
 
-builder.ConfigureFunctionsWebApplication();
-
-// Application Insights isn't enabled by default. See https://aka.ms/AAt8mw4.
-// builder.Services
-//     .AddApplicationInsightsTelemetryWorkerService()
-//     .ConfigureFunctionsApplicationInsights();
-
-builder.Build().Run();
+host.Run();
